5680080e22186d66547e02c4c5683052ef64e979
Gabryelle Duarte <gabryelleddev@gmail.com>
2025-04-25 23:03:14 -0300

    Create Aluno.pas

---
+++ pascal/record/Aluno.pas
@@ -0,0 +1,10 @@
+Program Aluno;
+var aluno record
+    nome: string[100]
+    cpf: string[14];
+    matricula: string[20];
+    idade: Integer;
+    endereco: string[150];
+    filiacao mae: string[100];
+    filiacao pai: string(100);
+end;
